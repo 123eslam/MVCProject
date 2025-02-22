603784cfb2c1404f578cf92f0b1b5a700c1bcafe
123eslam <eslameltamalawy124@gmail.com>
2025-02-23 00:41:46 +0200

    Video 4 - DAL - Department Repository

---
+++ Demo.DAL/Presistance/Repostories/Departmemts/IDepartmentRepository.cs
@@ -0,0 +1,18 @@
+﻿using Demo.DAL.Entities.Departments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Presistance.Repostories.Departmemts
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAll(bool AsNoTracking = true);
+        Department? GetByID(int id);
+        int AddDepartment(Department entity);
+        int UpdateDepartment(Department entity);
+        int DeleteDepartment(Department entity);
+    }
+}

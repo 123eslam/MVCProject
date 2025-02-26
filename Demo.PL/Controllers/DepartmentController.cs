c09c01293fce8ce1f2b5cc59b8dc5d3b37ba5bd2
123eslam <eslameltamalawy124@gmail.com>
2025-02-26 17:06:04 +0200

    Video 1 - Department Controller - Index

---
+++ Demo.PL/Controllers/DepartmentController.cs
@@ -0,0 +1,20 @@
+﻿using Demo.BLL.Services.Departments;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.PL.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentService _departmentService;
+        public DepartmentController(IDepartmentService departmentService)
+        {
+            _departmentService = departmentService;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var departments = _departmentService.GetDepartments();
+            return View(departments);
+        }
+    }
+}

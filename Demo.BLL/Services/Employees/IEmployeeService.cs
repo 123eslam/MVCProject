9b4ebf927112586cadfbb805beb6700e987a63bb
123eslam <eslameltamalawy124@gmail.com>
2025-03-03 08:43:15 +0200

    Video 3 - Employee Module - Service

---
+++ Demo.BLL/Services/Employees/IEmployeeService.cs
@@ -0,0 +1,13 @@
+﻿using Demo.BLL.Dtos.Employees;
+
+namespace Demo.BLL.Services.Employees
+{
+    public interface IEmployeeService
+    {
+        IEnumerable<EmployeeDto> GetEmployees();
+        EmployeeDetailesDto? GetEmployeeById(int id);
+        int CreateEmployee(CreateEmployeeDto entity);
+        int UpdateEmployee(UpdateEmployeeDto entity);
+        bool DeleteEmployee(int id);
+    }
+}

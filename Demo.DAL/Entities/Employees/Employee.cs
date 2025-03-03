0ff93f22c72b953dc134a859aeae785318e8c3c7
123eslam <eslameltamalawy124@gmail.com>
2025-03-03 06:54:59 +0200

    Video 1 - Employee Module - Entities, Configs, Migration

---
+++ Demo.DAL/Entities/Employees/Employee.cs
@@ -0,0 +1,19 @@
+﻿using Demo.DAL.Common.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Demo.DAL.Entities.Employees
+{
+    public class Employee : ModelBase
+    {
+        public string Name { get; set; } = null!;
+        public int? Age { get; set; }
+        public string? Address { get; set; }
+        public decimal Salary { get; set; }
+        public bool IsActive { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public DateTime HiringDate { get; set; }
+        public Gender Gender { get; set; }
+        public EmployeeType EmployeeType { get; set; }
+    }
+}

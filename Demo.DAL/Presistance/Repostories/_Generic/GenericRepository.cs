7ec4e14956ca53d6dfabae13606b097e5b9b9a3a
123eslam <eslameltamalawy124@gmail.com>
2025-03-03 07:35:22 +0200

    Video 2 - Employee Module - Repository

---
+++ Demo.DAL/Presistance/Repostories/_Generic/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using Demo.DAL.Entities;
+using Demo.DAL.Presistance.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.DAL.Presistance.Repostories._Generic
+{
+    public class GenericRepository<T> where T : ModelBase    
+    {
+        private protected readonly ApplicationDbContext _dbContext;
+
+        public GenericRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IEnumerable<T> GetAll(bool AsNoTracking = true)
+        {
+            if (AsNoTracking)
+                return _dbContext.Set<T>().AsNoTracking().ToList();
+            return _dbContext.Set<T>().ToList();
+        }
+        public IQueryable<T> GetAllQueryable()
+        {
+            return _dbContext.Set<T>();
+        }
+        public T? GetByID(int id)
+        {
+            //return _dbContext.T.Local.FirstOrDefault(D => D.Id == id);
+            return _dbContext.Set<T>().Find(id);
+        }
+        public int Add(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            return _dbContext.SaveChanges();
+        }
+        public int Update(T entity)
+        {
+            _dbContext.Set<T>().Update(entity);
+            return _dbContext.SaveChanges();
+        }
+        public int Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            return _dbContext.SaveChanges();
+        }
+    }
+}
---
+++ Demo.DAL/Presistance/Repostories/_Generic/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using Demo.DAL.Entities;
+
+namespace Demo.DAL.Presistance.Repostories._Generic
+{
+    public interface IGenericRepository<T> where T : ModelBase
+    {
+        IEnumerable<T> GetAll(bool AsNoTracking = true);
+        IQueryable<T> GetAllQueryable();
+        T? GetByID(int id);
+        int Add(T entity);
+        int Update(T entity);
+        int Delete(T entity);
+    }
+}

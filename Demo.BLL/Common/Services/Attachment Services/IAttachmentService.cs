b2d981c5c282acc0bace0309cc598bc2af63caa2
123eslam <eslameltamalawy124@gmail.com>
2025-03-19 14:16:41 +0200

    Video 5 - Attachment Service

---
+++ Demo.BLL/Common/Services/Attachment Services/IAttachmentService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.BLL.Common.Services.Attachment_Services
+{
+    public interface IAttachmentService
+    {
+        //(Upload - Delete) Image
+        public string? Upload(IFormFile file, string folderName);
+        public bool Delete(string filePath);
+    }
+}

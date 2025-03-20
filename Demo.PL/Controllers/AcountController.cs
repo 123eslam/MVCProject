75a1e1e65b2fbaf8ac1267d262ad055567f78643
123eslam <eslameltamalawy124@gmail.com>
2025-03-20 15:58:45 +0200

    Video 4 - Account Controller

---
+++ Demo.PL/Controllers/AcountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.PL.Controllers
+{
+    public class AcountController : Controller
+    {
+        //Register , Login , Signout
+        [HttpGet] //Display Register Form
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}

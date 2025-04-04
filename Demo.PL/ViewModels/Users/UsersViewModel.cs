5c82c868eead190f9e4c4c58857eca4890322c30
123eslam <eslameltamalawy124@gmail.com>
2025-04-04 22:45:06 +0200

    4 - UserController - Index

---
+++ Demo.PL/ViewModels/Users/UsersViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo.PL.ViewModels.Users
+{
+    public class UsersViewModel
+    {
+        public string Id { get; set; } = string.Empty;
+        public string FName { get; set; } = string.Empty;
+        public string LName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public IEnumerable<string> Roles { get; set; } = new List<string>();
+    }
+}
